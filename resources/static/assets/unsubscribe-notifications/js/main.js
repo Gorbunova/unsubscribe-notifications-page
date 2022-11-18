5504b9941ba61c32a73a1a2447b3955e1a7a1605
Gorbunova <alinagorbunova98@mail.ru>
2022-11-18 15:30:33 +0600

    Update main.js

--- resources/static/assets/unsubscribe-notifications/js/main.js
+++ resources/static/assets/unsubscribe-notifications/js/main.js
@@ -1,31 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-  setAdditionalSettings(document.querySelector('#flow_change_notify').checked)
+  setMainSettings(document.querySelector('#notify_by_email').checked, true)
+  setAdditionalSettings(document.querySelector('#flow_change_notify').checked, true)
   document.querySelector('#notify_by_email').addEventListener('change', (e) => setMainSettings(e.target.checked))
   document.querySelector('#flow_change_notify').addEventListener('change', (e) => setAdditionalSettings(e.target.checked))
 })
 
-function setMainSettings(checked) {
+function setMainSettings(checked, isFirstSetting = false) {
   if (!checked) {
-    document.querySelectorAll('input:not(#notify_by_email)').forEach(elem => {
+    document.querySelectorAll('input:not(#notify_by_email):not(#daily_notify)').forEach(elem => {
       elem.disabled = true
       elem.checked = false
     })
   } else {
-    document.querySelectorAll('input[type=checkbox]:not(#notify_by_email)').forEach(e => {
+    document.querySelectorAll('input[type=checkbox]:not(#notify_by_email):not(#daily_notify)').forEach(e => {
       e.disabled = false
-      e.checked = true
+      if (!isFirstSetting) e.checked = true
     })
     document.querySelectorAll('input[type=radio]').forEach(e => {
       e.disabled = false
-      if (e.id === 'any_changes') e.checked = true
+      if (!isFirstSetting) e.checked = e.id === 'any_changes'
     })
   }
 }
 
-function setAdditionalSettings(checked) {
+function setAdditionalSettings(checked, isFirstSetting = false) {
   document.querySelectorAll('input[type=radio]').forEach(e => {
     e.disabled = !checked
-    e.checked = checked ? e.id === 'any_changes' : checked
+    if (!isFirstSetting) e.checked = checked ? e.id === 'any_changes' : checked
   })
 }
 
